3fb628f12996bbbc2563e0ce2372b724b7c2b42a
Legend <me@liux.pro>
2022-02-04 21:09:00 +0800

    jdbc okhttp

--- src/main/java/pro/liux/web/controller/TestController.java
+++ src/main/java/pro/liux/web/controller/TestController.java
@@ -1,18 +1,29 @@
 package pro.liux.web.controller;
 
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.liux.web.client.DateTestClient;
 import pro.liux.web.vo.VditorImage;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
     @Autowired
     DateTestClient dateTestClient;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @PostMapping("aaa")
     public VditorImage aaa() {
@@ -25,4 +36,26 @@ public class TestController {
 
         return build;
     }
+
+    @GetMapping("db")
+    public Map testDB() {
+        Map<String, Object> stringObjectMap =
+                jdbcTemplate.queryForMap("select * from city");
+
+        return stringObjectMap;
+    }
+
+    @GetMapping("okhttp")
+    public Object testHttp(@RequestParam("url") String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string() + "";
+        }
+    }
+
 }
