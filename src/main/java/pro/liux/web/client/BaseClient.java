89f0bf81fe556063099ef3746d66d70eb161b32c
Legend <me@liux.pro>
2022-02-05 12:51:06 +0800

    改造签名

--- src/main/java/pro/liux/web/client/BaseClient.java
+++ src/main/java/pro/liux/web/client/BaseClient.java
@@ -2,26 +2,30 @@ package pro.liux.web.client;
 
 import lombok.Data;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestTemplate;
+import pro.liux.web.utils.AWSSignatureVersion4;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class BaseClient {
     private RestTemplate restTemplate = new RestTemplate();
-    private String url="http://www.httpbin.org/post?abc=xyz";
+    private String url = "http://localhost:9000/liux-pro";
+
     {
+//        url = "http://www.httpbin.org/get";
         restTemplate.setInterceptors(List.of(new Hook()));
     }
 
     public Boolean uploadFile(byte[] bytes) {
-        String s = restTemplate.postForObject(url, "bytes".getBytes(StandardCharsets.UTF_8), String.class);
+        String s = restTemplate.getForObject(url, String.class);
         System.out.println(s);
 
 
@@ -33,8 +37,19 @@ public class BaseClient {
         @NotNull
         @Override
         public ClientHttpResponse intercept(@NotNull HttpRequest request, @NotNull byte[] body, @NotNull ClientHttpRequestExecution execution) throws IOException {
-            request.getHeaders();
-            System.out.println(request.getURI().getQuery());
+            AWSSignatureVersion4.WebEntry webEntry = new AWSSignatureVersion4.WebEntry(
+                    request.getHeaders(),
+                    request.getMethodValue(),
+                    request.getURI().getAuthority(),
+                    request.getURI().getRawQuery(),
+                    request.getURI().getPath(),
+                    body
+            );
+
+            Map<String, List<String>> calc = new AWSSignatureVersion4().calc(webEntry);
+            System.out.println("calc = " + calc);
+            HttpHeaders headers = request.getHeaders();
+            calc.forEach(headers::addAll);
             return execution.execute(request, body);
         }
     }
--- src/main/java/pro/liux/web/utils/AWSSignatureVersion4.java
+++ src/main/java/pro/liux/web/utils/AWSSignatureVersion4.java
@@ -13,24 +13,14 @@
  */
 package pro.liux.web.utils;
 
-import feign.RequestInterceptor;
-import feign.RequestTemplate;
-import feign.Target;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.SneakyThrows;
+import lombok.*;
 import org.bouncycastle.util.Strings;
 import org.springframework.util.StringUtils;
 import org.springframework.util.comparator.Comparators;
 import pro.liux.web.config.SpringNativeFeignConfiguration;
 
-import java.net.URI;
-import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
-import java.util.TimeZone;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -38,7 +28,7 @@ import java.util.stream.Collectors;
 // http://docs.aws.amazon.com/general/latest/gr/signature-version-4.html
 @Getter
 @Setter
-public class AWSSignatureVersion4 implements RequestInterceptor {
+public class AWSSignatureVersion4 {
     private static final String EMPTY_STRING_HASH =
             "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
 
@@ -64,24 +54,24 @@ public class AWSSignatureVersion4 implements RequestInterceptor {
         this.endpoint = SpringNativeFeignConfiguration.endpoint;
     }
 
-    private static String canonicalString(RequestTemplate input, String signedHeaders, String hash) {
+    private static String canonicalString(WebEntry input, String signedHeaders, String hash) {
         StringBuilder canonicalRequest = new StringBuilder();
         // 请求方式（GET POST） + '\n' +
-        canonicalRequest.append(input.method()).append('\n');
+        canonicalRequest.append(input.getMethod().toUpperCase()).append('\n');
         // 域名后面的部分，不算get参数，以/开头
         // CanonicalURI + '\n' +
-        canonicalRequest.append(URI.create(input.url()).getPath()).append('\n');
+        canonicalRequest.append(input.getUriPath()).append('\n');
         //
         // CanonicalQueryString + '\n' +
-        if (StringUtils.hasText(input.queryLine())) {
+        if (StringUtils.hasText(input.getQueryString())) {
             //去除问号
-            canonicalRequest.append(input.queryLine().substring(1));
+            canonicalRequest.append(input.getQueryString());
         }
         canonicalRequest.append('\n');
 
 
         //所有header
-        String canonicalHeaders = input.headers().entrySet().stream()
+        String canonicalHeaders = input.getHeaders().entrySet().stream()
                 //key转成小写
                 .collect(Collectors.toMap(key -> key.getKey().toLowerCase()
                         , Map.Entry::getValue)).entrySet().stream()
@@ -115,14 +105,23 @@ public class AWSSignatureVersion4 implements RequestInterceptor {
         return toSign.toString();
     }
 
+    /**
+     * 封装Headers和请求
+     */
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class WebEntry {
+        private Map<String, List<String>> headers;
+        private String method;
+        private String host;
+        private String queryString;
+        private String uriPath;
+        private byte[] body;
+    }
 
-    @Override
-    @SneakyThrows
-    public void apply(RequestTemplate input) {
-        Target<?> target = input.feignTarget();
-
-        URL url = new URL(target.url());
-        String host = url.getAuthority();
+    public Map<String, List<String>> calc(WebEntry input) {
+        Map<String, List<String>> headers = new HashMap<>();
 
         String timestamp;
         synchronized (iso8601) {
@@ -133,19 +132,19 @@ public class AWSSignatureVersion4 implements RequestInterceptor {
                 String.format("%s/%s/%s/%s", timestamp.substring(0, 8), region, service, "aws4_request");
 
 
-        byte[] body = input.body();
+        byte[] body = input.getBody();
         String hash;
         if (body != null) {
             hash = EncryptUtils.hex(EncryptUtils.sha256(body));
         } else {
             hash = EMPTY_STRING_HASH;
         }
-        input.header("x-amz-content-sha256", hash);
-        input.header("Host", host);
-        input.header("x-amz-date", timestamp);
+        input.getHeaders().put("x-amz-content-sha256", List.of(hash));
+        input.getHeaders().put("Host", List.of(input.getHost()));
+        input.getHeaders().put("x-amz-date", List.of(timestamp));
 
         //所有key小写排序，分号分割，拼在一起
-        String signedHeaders = input.headers().keySet().stream().sorted(Comparators.comparable())
+        String signedHeaders = input.getHeaders().keySet().stream().sorted(Comparators.comparable())
                 .map(String::toLowerCase).collect(Collectors.joining(";"));
 
         String canonicalString = canonicalString(input, signedHeaders, hash);
@@ -155,7 +154,8 @@ public class AWSSignatureVersion4 implements RequestInterceptor {
         String signature = EncryptUtils.hex(EncryptUtils.hmacSHA256(toSign, signatureKey));
         String authorization = String.format("AWS4-HMAC-SHA256 Credential=%s/%s,SignedHeaders=%s,Signature=%s"
                 , accessKey, credentialScope, signedHeaders, signature);
-        input.header("Authorization", authorization);
+        input.getHeaders().put("Authorization", List.of(authorization));
+        return headers;
     }
 
     byte[] signatureKey(String secretKey, String timestamp) {
