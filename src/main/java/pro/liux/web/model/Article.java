bcb24803470714fff8cfc7389b1b5143aa05a344
Legend <me@liux.pro>
2022-02-23 17:20:28 +0800

    fix redis

--- src/main/java/pro/liux/web/model/Article.java
+++ src/main/java/pro/liux/web/model/Article.java
@@ -3,9 +3,11 @@ package pro.liux.web.model;
 import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.data.redis.core.RedisHash;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@RedisHash("article")
 public class Article extends BaseModel {
     private String title;
 
